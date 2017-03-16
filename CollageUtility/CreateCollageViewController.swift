1ec708cffdaaece34c0586b2cc2d8feeb9b75a26
Nate Gray <ngray311@gmail.com>
2017-03-15 23:00:20 -0400

    Initial UI

---
+++ CollageUtility/CreateCollageViewController.swift
@@ -0,0 +1,63 @@
+//
+//  CreateCollageViewController.swift
+//  CollageUtility
+//
+//  Created by Gray, Nathan on 3/15/17.
+//  Copyright © 2017 Gray, Nathan. All rights reserved.
+//
+
+import UIKit
+
+class CreateCollageViewController: UIViewController {
+
+    //MARK: Outlets
+    
+    @IBOutlet weak var numberCount: UILabel!
+    
+    
+    //MARK: Actions
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    //MARK: Picker Functions
+    
+//    let pickerData = [2, 3, 4, 5]
+    
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        return 1
+//    }
+//    
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        return pickerData.count
+//    }
+//    
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> Int {
+//        return pickerData[row]
+//    }
+//    
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        
+//    }
+
+    
+}
